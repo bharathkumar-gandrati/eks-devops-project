42c5f5c1393c2c630c830b213cdbff8fbca655f7
Bharath Gandrati <bharath.g@smartgig.tech>
2024-04-26 16:51:02 +0530

    Add sg rule for eks

--- terraform/modules/eks/securitygroups.tf
+++ terraform/modules/eks/securitygroups.tf
@@ -12,6 +12,14 @@ resource "aws_security_group" "worker_node_sg" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+   ingress {
+    description      = "ssh access to public"
+    from_port        = 30082
+    to_port          = 30082
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
