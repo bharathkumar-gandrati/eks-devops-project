0c22c9220dfc035e0f82d027b8a9f1996c1dbd9a
Bharath Gandrati <bharath.g@smartgig.tech>
2024-04-29 12:53:20 +0530

    Actions Change4

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -3,6 +3,12 @@
 # Change directory to the location of the script
 cd "$(dirname "$0")"
 
-kubectl apply -f namespace.yaml
-kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
+# Define paths to YAML files relative to the script's location
+NAMESPACE_FILE="namespace.yaml"
+DEPLOYMENT_FILE="deployment.yaml"
+SERVICE_FILE="service.yaml"
+
+# Apply Kubernetes resources using the defined paths
+kubectl apply -f "$NAMESPACE_FILE"
+kubectl apply -f "$DEPLOYMENT_FILE"
+kubectl apply -f "$SERVICE_FILE"
