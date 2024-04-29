3749119b5080f856674d464c52e650d6d335842d
Bharath Gandrati <bharath.g@smartgig.tech>
2024-04-29 12:50:15 +0530

    Actions Change3

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-kubectl apply -f ./namespace.yaml
-kubectl apply -f ./deployment.yaml
-kubectl apply -f ./service.yaml
+
+# Change directory to the location of the script
+cd "$(dirname "$0")"
+
+kubectl apply -f namespace.yaml
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
